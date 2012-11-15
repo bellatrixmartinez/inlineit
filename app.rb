563b6f374d2e66b54d4e8fbcb8b715e8b1e0c24b
bellatrix@bakedweb.net <bellatrix@bakedweb.net>
2012-11-15 13:06:29 -0600

    style

--- app.rb
+++ app.rb
@@ -1,19 +1,19 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
-require 'hpricot'
 require 'tamtam'
 
 get '/' do
   haml :index
 end
 
-
 post '/work' do
   if params[:html].nil?
-    "Ups... Did you sent us an HTML file?"
+    @htmlerror="Ups... Did you sent us an HTML file?"
+    haml :error
   elsif params[:css].nil?
-    "Ups... def Ups... Did you sent us an CSS file?"
+    @csserror="Ups... Did you sent us an CSS file?"
+    haml :error
   else
     @html = params[:html][:tempfile].read
     @css =  params[:css][:tempfile].read
